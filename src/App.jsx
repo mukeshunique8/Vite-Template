7f0a04f278638dcdf3d3ecee6258ff0e0dc5fda1
mukeshunique8 <muthu888focus@gmail.com>
2024-04-03 09:31:59 +0530

    Edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom";
-import Navbar from "./components/Navbar";
-import Card from "./components/Card";
-import data from "./dummy.json"; // Import your JSON data
-import MemeCard from "./components/MemeCard"
-import ProfileCard from "./components/ProfileCard";
-import Todo from "./components/Todo";
-function App() {
-  
-  // console.log(data);
-  const mappedCards = data.map((item, index) => (
-    <Card 
-      key={index}
-      imgurl={item.imageUrl}
-      rating={item.rating}
-      totalRatings={item.totalRatings}
-      location={item.location}
-      title={item.title}
-      price={item.price}
-      soldOut={item.soldOut}
-    />
-  ))
-  console.log(mappedCards);
+import Navbar from './components/Navbar'
+
+
+export default function App() {
   return (
-    <div className="w-full h-screen">
-     
-      <Navbar />
-
-      <div className="flex flex-col justify-center sm:flex-row flex-wrap">
-      {mappedCards}
-
-      </div>
-
-      <MemeCard></MemeCard>
-
-
-      {/* <Todo></Todo> */}
-      {/* <ProfileCard/> */}
+    <div>
+      <Navbar></Navbar>
     </div>
-  );
+  )
 }
+
 
 // Use createRoot to render the App component asynchronously
 const root = createRoot(document.getElementById("root"));
